2066c74905201065cd552fd704d8976dcff10824
oscr <oscar.utbult@gmail.com>
2013-06-23 22:57:05 +0200

    Solved problem 21

---
+++ 021.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+from math import sqrt
+
+
+def getDivisorSum(n):
+    sum = 1
+    for i in range(2, int(sqrt(n) + 1)):
+        if n % i == 0:
+            sum += i
+            sum += n // i
+
+    return sum
+
+
+def getAmicableSumLimit10000():
+    divisorSums = {}
+    for i in range(1, 10000):
+        divisorSums[i] = getDivisorSum(i)
+
+    sum = 0
+    for (i, j) in divisorSums.items():
+        if i != j and divisorSums.get(j) == i:
+            sum += i
+
+    return sum
+
+
+print(getAmicableSumLimit10000())
