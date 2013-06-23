cca6f9ae75533774e88b8995e4b84e079206d59f
oscr <oscar.utbult@gmail.com>
2013-06-23 14:12:21 +0200

    Fixed import for Python3

--- 005.py
+++ 005.py
@@ -2,5 +2,6 @@
 
 
 # Lambda calculates the smallest positive integer that both n and m divide.
+from functools import reduce
 from fractions import gcd
 print(reduce(lambda n, m: (m * n) // gcd(m, n), [i for i in range(1, 21)], 1))
