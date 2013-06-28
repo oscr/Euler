3b765a0e98ac8ce700ad5132f679645be85431ec
oscr <oscar.utbult@gmail.com>
2013-06-28 12:59:30 +0200

    Added Java solution

---
+++ 007/FindPrime.java
@@ -0,0 +1,24 @@
+public class FindPrime {
+
+	public static void main(String[] args) {
+		int primesLeftToFind = 10000;
+		int maybePrime = 1;
+		do {
+			maybePrime += 2;
+			if (isPrime(maybePrime)) {
+				primesLeftToFind--;
+			}
+		} while (primesLeftToFind > 0);
+		System.out.println(maybePrime);
+	}
+
+	public static boolean isPrime(int n) {
+		for (int i = 2; i < (int) Math.pow(n, 0.5) + 1; i++) {
+			if (n % i == 0) {
+				return false;
+
+			}
+		}
+		return true;
+	}
+}
