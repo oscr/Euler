47d38be105b7485645ca8ee2feef8f03218278b5
oscr <oscar.utbult@gmail.com>
2013-07-26 10:20:03 +0200

    Improved Scala solution

--- 001/001.scala
+++ 001/001.scala
@@ -1,4 +1,4 @@
 object HelloWorld {
     def main(args: Array[String]) =
-        println((1 until 1000).filter(n => n % 3 == 0 || n % 5 == 0).sum)
+        println (1 until 1000 filter(n => n % 3 == 0 || n % 5 == 0) sum)
 }
